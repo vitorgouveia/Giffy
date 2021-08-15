f034dc6a10a4e1fa1199131b76c07b5d433beaa2
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-15 02:55:51 -0300

    feat(tutorial): add & conditionally renders tutorial component

---
+++ src/screen/Tutorial/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+import { TutorialContainer } from './styles'
+
+export const Tutorial: FC = () => {
+  return (
+    <TutorialContainer>
+      <h1>tutorial</h1>
+    </TutorialContainer>
+  )
+}
--- src/screen/index.tsx
+++ src/screen/index.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import Header from './Header'
+import { History } from './History'
+import { RecordArea } from './Main'
 import { Container, Content } from './styles'
+import { Tutorial } from './Tutorial'
 
-const screen: React.FC = () => {
+const Screen: FC = () => {
+  const [isFirstUser, setIsFirstUser] = useState(false)
+
+  useEffect(() => {
+    const hasTakenTour = localStorage.getItem('tookTutorial') === 'true'
+
+    if (hasTakenTour) {
+      setIsFirstUser(false)
+    } else {
+      localStorage.setItem('tookTutorial', 'true')
+      setIsFirstUser(true)
+    }
+    setIsFirstUser(true)
+  }, [])
   return (
     <Container>
-      <Header title="Giffy" />
-      <Content>
-        <h1>helloooo</h1>
+      <Header title="Giffy - Main" />
+      <Content isTutorial={isFirstUser}>
+        {isFirstUser ? (
+          <Tutorial />
+        ) : (
+          <>
+            <RecordArea />
+            <History />
+          </>
+        )}
       </Content>
     </Container>
   )
 }
 
-export default screen
+export default Screen
