db8c941c97aac6abbfe8bfbd1ce83a364e935102
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-15 02:55:04 -0300

    feat(history): add history component

---
+++ src/screen/History/index.tsx
@@ -0,0 +1,75 @@
+import React, { FC } from 'react'
+
+import { HistoryContainer, Card } from './styles'
+
+export const History: FC = () => {
+  return (
+    <HistoryContainer>
+      <h1>Your gif history</h1>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+      <Card tabIndex={0}>
+        <header>
+          <div className="img"></div>
+          <h5>name of the file</h5>
+          <p>Tuesday, 23, 12:00</p>
+        </header>
+        <small>file://dawee</small>
+      </Card>
+    </HistoryContainer>
+  )
+}
