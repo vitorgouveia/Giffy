f709daa0728f021fc8edc0f66ee399225f9197e0
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-01-24 19:28:05 +0000

    Deploying to gh-pages from @ VitorGouveia/Giffy@13c450d8900bfbe6579e9b2efa13415414efdfc0 🚀

--- _next/static/ZTQeu-cMke_Und0LoC1to/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ea6ac712083a40dd.js"],"/_error":["static/chunks/pages/_error-1b64a3ea4e93145e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
