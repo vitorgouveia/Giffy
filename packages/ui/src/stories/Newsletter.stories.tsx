944b06fc8ea8a958bdca0ecc6c3821a7f8a8bbd7
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-01-25 05:00:33 -0300

    feat(ui): add Blog Newsletter component

---
+++ packages/ui/src/stories/Newsletter.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { Newsletter, NewsletterProps } from '../components/Newsletter'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+export default {
+  title: 'Blog/Newsletter',
+  component: Newsletter,
+} as ComponentMeta<typeof Newsletter>
+
+const Template: ComponentStory<typeof Newsletter> = (args: NewsletterProps) => (
+  <Newsletter {...args} />
+)
+
+export const Primary = Template.bind({})
+Primary.args = {}
