77becdf58abe96f9eed72fd049f35b65181dcf2e
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-01-29 02:40:04 -0300

    feat(ui): add actions addon to storybook

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -6,6 +6,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-viewport',
+    '@storybook/addon-actions',
   ],
   framework: '@storybook/react',
   webpackFinal: async (config, { configType }) => {
