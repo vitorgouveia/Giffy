2e6f7fde13f4375ef0e949f45adc8132a07f9c47
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-01-30 13:20:56 -0300

    fix(styles): comment out oldie postcss plugin

--- packages/styles/postcss.config.js
+++ packages/styles/postcss.config.js
@@ -1,26 +1,10 @@
-// module.exports = {
-//   map: {
-//     sourcesContent: true,
-//     annotation: true,
-//   },
-//   syntax: 'postcss-scss',
-//   plugins: [
-//     require('postcss-reporter'),
-//     require('autoprefixer'),
-//     require('cssnano'),
-//     require('postcss-at-else'),
-//     require('oldie'),
-//     require("postcss-preset-env")
-//   ],
-// }
-
 module.exports = {
   plugins: [
     require('postcss-reporter'),
     require('autoprefixer'),
     require('cssnano'),
     require('postcss-at-else'),
-    require('oldie'),
+    // require('oldie'),
     require('postcss-preset-env'),
     require('postcss-color-hsla-fallback'),
     require('postcss-prefixer')({
