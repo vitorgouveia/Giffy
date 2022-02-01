6a67d425924a20e7520c551f7dc935b804a80943
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-01 18:13:07 -0300

    fix(web): correctly prefixes elements and fix hot-reload issue

--- apps/web/src/utils/prefixClassNames.ts
+++ apps/web/src/utils/prefixClassNames.ts
@@ -4,11 +4,14 @@ export const prefixClassNames = () => {
     'head',
     'head *',
     'body',
+    'svg',
+    'svg *',
     '#__next',
     'script',
     '#__next-build-watcher',
     'next-route-announcer',
     '#__next-route-announcer__',
+    '[data-styled]',
   ]
 
   const prefix = 'giffy_css'
@@ -17,15 +20,22 @@ export const prefixClassNames = () => {
 
   if (typeof window !== 'undefined') {
     elements.forEach(element => {
-      const { className } = element
- 
-      if (!!className) {
-        if (!className.includes(prefix)) {
-          element.className = `${prefix}-${className}`
-        }
+      if (!element.className) {
+        return
       }
-    })
 
-    localStorage.setItem('@giffy:prefix', 'true')
+      const classnames = element.className.split(' ')
+      const newClasses = classnames.map(classname => {
+        const isStyledComponentss = classname.includes('styles')
+        const isAlreadyPrefixed = classname.includes('giffy_css-')
+        if (isStyledComponentss || isAlreadyPrefixed) {
+          return classname
+        } else {
+          return `${prefix}-${classname}`
+        }
+      })
+
+      element.className = newClasses.join(' ')
+    })
   }
 }
