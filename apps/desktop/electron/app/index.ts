194a9c0497c96bfe356eb7a47d9abd85cff05254
VitorGouveia <vitorneves.gouveia10@gmail.com>
2022-02-08 00:00:42 -0300

    feat(desktop): add start scripts

--- apps/desktop/electron/app/index.ts
+++ apps/desktop/electron/app/index.ts
@@ -1,3 +1,21 @@
+import { BrowserWindow } from 'electron'
+import path from 'path'
+
 export const App = () => {
+  const __isDev__ = process.env.NODE_ENV === 'development'
+
+  const window = new BrowserWindow({
+    width: 1200,
+    height: 700,
+  })
+
+  const client = 'http://localhost:8000'
+
+  if (__isDev__) {
+    window.loadURL(client)
+  } else {
+    window.loadFile(path.resolve(__dirname, '..', 'index.html'))
+  }
+
   console.log('app')
 }
